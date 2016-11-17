6244a245fc16f316ac558844636ddb39efbbbcfd
salamee <mindblowing10000@gmail.com>
2016-11-16 20:27:05 -0500

    Update key.js

--- key.js
+++ key.js
@@ -59,8 +59,8 @@ context.fillText("Remember you can only eat these small boxes, and eating them w
 context.fillText("But Beware of the boxes that are bigger than you, you cannot eat it, or else it'll be game over.",175,90);
 context.fillText("But you can eat these bigger boxes once you are bigger than the big boxes.",175,110);
 context.fillText("Eat all the boxes as fast as you can to win.",175,130);
-context.fillText("Remember!! The more time you take, the less points you have.",200,150);
-context.fillText("PRESS ANY KEY TO START",400,200)
+context.fillText("Remember!! The more time you take, the less points you have.",175,150);
+context.fillText("PRESS ANY KEY TO START",600,200)
 
 	//function, that takes care of the moving, no parameters, for it is an event
 	function move(e){
