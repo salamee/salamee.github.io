5933847b4af7a6b46c4df4c3fbd0702b851a8089
salamee <mindblowing10000@gmail.com>
2016-12-07 22:58:59 -0500

    Update key.js

--- key.js
+++ key.js
@@ -16,31 +16,21 @@ The ponts are clculated by subtracting the time take to play, and subtacting tha
 	//GETTING THINGS STARTED TO GET SET
 
 
-	//call on canvas and its context, created in the html file
-	//create a cavas variable and context variable
-	var Canvas = document.getElementById('canvas');
-	var context = Canvas.getContext('2d');
+	//variables
+	var Canvas;
+	var context;
+	var timeCounter;
+	var myMos = [];
+	var myBogs = [];
+	var xPos;
+	var yPos;
+	var width;
+	var height;
+	//variable of player's square
+	var userSquare;
 
-	//OUTPUT INSTRUCTIONS
-
-	//set font to 18pt georgia
-	context.font="18px Georgia";
-	//set font color to blue
-	context.fillStyle = "blue";
-	//fill the convas to text with instruction
-	//with centre positions
-	context.fillText("Use your arrow keys to move your box, the one on the left corner. Move this box to eat the smaller boxes.",225,50); 
-	context.fillText("Remember you can only eat these small boxes, and eating them will make your box bigger.",275,70); 
-	context.fillText("But Beware of the boxes that are bigger than you, you cannot eat it, or else it'll be game over.",270,90);
-	context.fillText("But you can eat these bigger boxes once you are bigger than the big boxes.",350,110);
-	context.fillText("Eat all the boxes as fast as you can to win.",450,130);
-	context.fillText("Remember!! The more time you take, the less points you have.",380,150);
-	context.fillText("PRESS ANY KEY TO START",500,200)
 
 	//TIMER
-
-	//start time variable at zero
-	var timeCounter=0;
 	//function for timer, timing the game
 	function time(){
 		//add one to time variable
@@ -50,6 +40,7 @@ The ponts are clculated by subtracting the time take to play, and subtacting tha
 		//set the text to the current time counted, for the time id
 		document.getElementById('timer').innerHTML = timeCounter;
 	}
+
 
 	//REDRAW CANVAS
 
@@ -80,14 +71,6 @@ The ponts are clculated by subtracting the time take to play, and subtacting tha
 		}	
 	}
 
-
-
-
-	//CREATING THE OBJECTS
-
-
-
-
 	//CREATING SQUARES FUNCTION
 
 	//function that takes parameters of x and y position, width and height, and fill colour
@@ -99,21 +82,45 @@ The ponts are clculated by subtracting the time take to play, and subtacting tha
   		this.h = h;
 	}
 
-//x and y positon of player's square, start at zero and zero position
-	var xPos = 0;
-	var yPos = 0;
-	//the width and height of the players square
-	var width = 15;
-	var height = 15;
-	//variable of player's square
-	var userSquare;
-var myMos = [];
-	var myBogs = [];
+
+
+
 function play(){
 
-	//CREATING THE ENEMY SQUARES
+	//call on canvas and its context, created in the html file
+	//create a cavas variable and context variable
+	Canvas = document.getElementById('canvas');
+	context = Canvas.getContext('2d');
 
+	//OUTPUT INSTRUCTIONS
+
+	//set font to 18pt georgia
+	context.font="18px Georgia";
+	//set font color to blue
+	context.fillStyle = "blue";
+	//fill the convas to text with instruction
+	//with centre positions
+	context.fillText("Use your arrow keys to move your box, the one on the left corner. Move this box to eat the smaller boxes.",225,50); 
+	context.fillText("Remember you can only eat these small boxes, and eating them will make your box bigger.",275,70); 
+	context.fillText("But Beware of the boxes that are bigger than you, you cannot eat it, or else it'll be game over.",270,90);
+	context.fillText("But you can eat these bigger boxes once you are bigger than the big boxes.",350,110);
+	context.fillText("Eat all the boxes as fast as you can to win.",450,130);
+	context.fillText("Remember!! The more time you take, the less points you have.",380,150);
+	context.fillText("PRESS ANY KEY TO START",500,200)
+
+	//TIMER
+
+	//start time variable at zero
+	timeCounter=0;
 	
+
+
+	//CREATING THE OBJECTS
+
+
+
+
+	//CREATING THE ENEMY SQUARES
 
 	var bob=0;
 	while(bob<20){
@@ -133,8 +140,14 @@ function play(){
 
 	//CREATING THE PLAYERS SQUARE
 
+	//x and y positon of player's square, start at zero and zero position
+	xPos = 0;
+	yPos = 0;
+	//the width and height of the players square
+	width = 15;
+	height = 15;
+	//variable of player's square
 	
-
 	// create a rectangle variables holder, (using function shape)
 	//with the width, and height of the variables of width and height, and with x and y postions, at xPos and yPos variables, and fill colour.
 	//that is the users square
