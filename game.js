02380ba63fb397a383d20fce89c8736d8547bba3
salamee <mindblowing10000@gmail.com>
2022-07-01 01:22:18 -0400

    Update game.js

--- game.js
+++ game.js
@@ -87,8 +87,8 @@ The ponts are clculated by subtracting the time take to play, and subtacting tha
 		context.fillText("Remember you can only eat these small boxes, and eating them will make your box bigger.",275,70); 
 		context.fillText("But Beware of the boxes that are bigger than you, you cannot eat it, or else it'll be game over.",270,90);
 		context.fillText("But you can eat these bigger boxes once you are bigger than the big boxes.",350,110);
-		context.fillText("Eat all the boxes as fast as you can to win. Move Carefully",380,130);
-		context.fillText("Remember!! The more time you take, the less points you have.",380,150);
+		context.fillText("Eat all the boxes as fast as you can to win. Move carefully!",380,130);
+		context.fillText("Remember, the more time you take, the less points you have!",380,150);
 		//set font to 22pt georgia bold
 		context.font="bold 22px Georgia";
 		context.fillText("PRESS ANY KEY TO START",455,200);
